5949377bb146b65ab973f49a578636dafd80fada
alimakouar <makouarali@gmail.com>
2022-03-18 12:59:27 +0100

    Update miseEnForme.js

---
+++ miseEnForme.js
@@ -0,0 +1,82 @@
+document.write(`
+<div class="qst">
+    <h2>Question 1<br></h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q1"><p><br></p>
+    <input type="radio" id="2" name="q1"><p><br></p>
+    <input type="radio" id="3" name="q1"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px"  onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 2</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q2"><p><br></p>
+    <input type="radio" id="2" name="q2"><p><br></p>
+    <input type="radio" id="3" name="q2"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 3</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q3"><p><br></p>
+    <input type="radio" id="2" name="q3"><p><br></p>
+    <input type="radio" id="3" name="q3"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 4</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q4"><p><br></p>
+    <input type="radio" id="2" name="q4"><p><br></p>
+    <input type="radio" id="3" name="q4"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 5</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q5"><p><br></p>
+    <input type="radio" id="2" name="q5"><p><br></p>
+    <input type="radio" id="3" name="q5"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 6</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q6"><p><br></p>
+    <input type="radio" id="2" name="q6"><p><br></p>
+    <input type="radio" id="3" name="q6"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name);"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 7</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q7"><p><br></p>
+    <input type="radio" id="2" name="q7"><p><br></p>
+    <input type="radio" id="3" name="q7"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 8</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q8"><p><br></p>
+    <input type="radio" id="2" name="q8"><p><br></p>
+    <input type="radio" id="3" name="q8"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)" onclick=""><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 9</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q9"><p><br></p>
+    <input type="radio" id="2" name="q9"><p><br></p>
+    <input type="radio" id="3" name="q9"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+<div class="qst">
+    <h2>Question 10</h2>
+    <p><br></p>
+    <input type="radio" id="1" name="q10"><p><br></p>
+    <input type="radio" id="2" name="q10"><p><br></p>
+    <input type="radio" id="3" name="q10"><p><br></p>
+    <button type="button" style="width:5%;height:5%;background:rgba(82, 78, 78, 0);border:solid 1px white;border-radius:20px" onclick="showAnswer(this.parentNode.parentNode.id,this.previousSibling.previousSibling.previousSibling.name)"><img src="assets/pics/hint.png" style="width:100%;height:100%;"></button>
+</div>
+`);
