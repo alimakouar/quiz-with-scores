0e070c799e7693fa01093caf1d7801892788dd9f
alimakouar <makouarali@gmail.com>
2022-03-18 13:00:20 +0100

    Update quizContent.js

---
+++ quizContent.js
@@ -0,0 +1,160 @@
+    
+var firstquizanswers = {"q1":1,"q2":3,"q3":1,"q4":2,"q5":1,"q6":3,"q7":2,"q8":3,"q9":1,"q10":2};
+
+//for the 1st quiz
+document.getElementsByClassName("qst")[0].childNodes[3].innerHTML='This is the 1st question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[0].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[0].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[0].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[1].childNodes[3].innerHTML='This is the 2nd question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[1].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[1].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[1].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[2].childNodes[3].innerHTML='This is the 3rd question, the right answer is 1:) <br>'
+document.getElementsByClassName("qst")[2].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[2].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[2].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[3].childNodes[3].innerHTML='This is the 4th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[3].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[3].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[3].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[4].childNodes[3].innerHTML='This is the 5th question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[4].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[4].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[4].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[5].childNodes[3].innerHTML='This is the 6th question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[5].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[5].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[5].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[6].childNodes[3].innerHTML='This is the 7th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[6].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[6].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[6].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[7].childNodes[3].innerHTML='This is the 8th question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[7].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[7].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[7].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[8].childNodes[3].innerHTML='This is the 9th question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[8].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[8].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[8].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[9].childNodes[3].innerHTML='This is the 10th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[9].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[9].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[9].childNodes[12].innerHTML='3rd answer<br>' 
+
+
+//for the 2nd quiz
+var secondquizanswers ={"q1":3,"q2":1,"q3":1,"q4":2,"q5":3,"q6":2,"q7":3,"q8":2,"q9":2,"q10":2};
+
+document.getElementsByClassName("qst")[10].childNodes[3].innerHTML='This is the first question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[10].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[10].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[10].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[11].childNodes[3].innerHTML='This is the 2nd question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[11].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[11].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[11].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[12].childNodes[3].innerHTML='This is the 3rd question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[12].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[12].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[12].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[13].childNodes[3].innerHTML='This is the 4th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[13].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[13].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[13].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[14].childNodes[3].innerHTML='This is the 5th question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[14].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[14].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[14].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[15].childNodes[3].innerHTML='This is the 6th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[15].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[15].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[15].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[16].childNodes[3].innerHTML='This is the 7th question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[16].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[16].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[16].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[17].childNodes[3].innerHTML='This is the 8th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[17].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[17].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[17].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[18].childNodes[3].innerHTML='This is the 9th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[18].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[18].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[18].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[19].childNodes[3].innerHTML='This is the 10th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[19].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[19].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[19].childNodes[12].innerHTML='3rd answer<br>' 
+
+//for the 3rd form
+var thirdquizanswers ={"q1":1,"q2":2,"q3":2,"q4":3,"q5":1,"q6":2,"q7":2,"q8":1,"q9":1,"q10":3};
+
+document.getElementsByClassName("qst")[20].childNodes[3].innerHTML='This is the first question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[20].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[20].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[20].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[21].childNodes[3].innerHTML='This is the 2nd question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[21].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[21].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[21].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[22].childNodes[3].innerHTML='This is the 3rd question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[22].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[22].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[22].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[23].childNodes[3].innerHTML='This is the 4th question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[23].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[23].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[23].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[24].childNodes[3].innerHTML='This is the 5th question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[24].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[24].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[24].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[25].childNodes[3].innerHTML='This is the 6th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[25].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[25].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[25].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[26].childNodes[3].innerHTML='This is the 7th question, the right answer is 2 :) <br>'
+document.getElementsByClassName("qst")[26].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[26].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[26].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[27].childNodes[3].innerHTML='This is the first question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[27].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[27].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[27].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[28].childNodes[3].innerHTML='This is the first question, the right answer is 1 :) <br>'
+document.getElementsByClassName("qst")[28].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[28].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[28].childNodes[12].innerHTML='3rd answer<br>' 
+
+document.getElementsByClassName("qst")[29].childNodes[3].innerHTML='This is the first question, the right answer is 3 :) <br>'
+document.getElementsByClassName("qst")[29].childNodes[6].innerHTML='1st answer<br>' 
+document.getElementsByClassName("qst")[29].childNodes[9].innerHTML='2nd answer<br>' 
+document.getElementsByClassName("qst")[29].childNodes[12].innerHTML='3rd answer<br>' 
